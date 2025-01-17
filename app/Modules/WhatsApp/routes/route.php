0604a81c3874176c344be90e1f62215b5f9b0cae
unknown <amr_elngm6@yahoo.com>
2025-01-17 09:20:48 +0200

    .

--- app/Modules/WhatsApp/routes/route.php
+++ app/Modules/WhatsApp/routes/route.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Modules\WhatsApp\Controllers\ConversationController;
+use App\Modules\WhatsApp\Controllers\MessageController;
 
 Route::prefix('wp_conversations')->middleware(['web','auth:staff,superadmin'])->group(function () {
     Route::get('/', [ConversationController::class, 'index'])->name('WhatsApp');
@@ -20,14 +21,14 @@ Route::prefix('wp_contacts')->middleware(['web','auth:staff,superadmin'])->group
 });
 
 Route::prefix('wp_messages')->middleware(['web','auth:staff,superadmin'])->group(function () {
-    Route::get('/', [ConversationController::class, 'index'])->name('WhatsMessages');
-    Route::get('/create', [ConversationController::class, 'create'])->name('WhatsMessage.create');
-    Route::get('/{id}/show', [ConversationController::class, 'show'])->name('WhatsMessage.show');
-    Route::get('/{id}/edit', [ConversationController::class, 'edit'])->name('WhatsMessage.edit');
-    Route::post('/', [ConversationController::class, 'store'])->name('WhatsMessage.store');
-    Route::post('/filter', [ConversationController::class, 'filter'])->name('WhatsMessage.filter');
-    Route::post('{id}/update', [ConversationController::class, 'update'])->name('WhatsMessage.update');
-    Route::delete('{id}/delete', [ConversationController::class, 'destroy'])->name('WhatsMessage.delete');
+    Route::get('/', [MessageController::class, 'index'])->name('WhatsMessages');
+    Route::get('/create', [MessageController::class, 'create'])->name('WhatsMessage.create');
+    Route::get('/{id}/show', [MessageController::class, 'show'])->name('WhatsMessage.show');
+    Route::get('/{id}/edit', [MessageController::class, 'edit'])->name('WhatsMessage.edit');
+    Route::post('/', [MessageController::class, 'store'])->name('WhatsMessage.store');
+    Route::post('/filter', [MessageController::class, 'filter'])->name('WhatsMessage.filter');
+    Route::post('{id}/update', [MessageController::class, 'update'])->name('WhatsMessage.update');
+    Route::delete('{id}/delete', [MessageController::class, 'destroy'])->name('WhatsMessage.delete');
 
 });
 
