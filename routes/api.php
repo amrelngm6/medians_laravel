8a5cfff9a73bf9ae85781d1fbb911ea515ebd71b
unknown <amr_elngm6@yahoo.com>
2025-01-17 07:56:05 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,4 @@ use App\Modules\WhatsApp\Controllers\MessageController;
 
 
 Route::post('/webhooks/medians_wp', [MessageController::class, 'webhook']);
-Route::get('/webhooks/medians_wp2', [MessageController::class, 'webhook']);
-Route::get('/api/webhooks/medians_wp2', [MessageController::class, 'webhook']);
+Route::get('/webhooks/medians_wp', [MessageController::class, 'webhook']);
