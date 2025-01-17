ad5f9c1de8360ab8ac95521844203bbcde822ed5
unknown <amr_elngm6@yahoo.com>
2025-01-17 03:59:47 +0200

    .

--- database/migrations/0001_11_10_024150_create_modules_table.php
+++ database/migrations/0001_11_10_024150_create_modules_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         
         Schema::create('modules', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
+            
+
             $table->string('name');
             $table->string('path');
             $table->string('provider');
