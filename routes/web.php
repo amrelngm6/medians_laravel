a9e4f428d0317ead497adab4e8710fc0e6c7a84d
unknown <amr_elngm6@yahoo.com>
2025-01-17 05:27:30 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -53,10 +53,7 @@ Route::get('', function () {
     // return view('dashboard.analytics');
 })->name('index');
 
-Route::get('webhooks/medians_wp', function () {
-    echo 'medians_wp';
-})->name('webhooks_wp');
-
+Route::get('webhooks/medians_wp', [MessageController::class, 'webhook'])->name('webhooks');
 Route::post('webhooks/medians_wp', [MessageController::class, 'webhook'])->name('webhooks');
 
 
