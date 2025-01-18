3331020323883dc275f840ec0603ef7a6a24b733
unknown <amr_elngm6@yahoo.com>
2025-01-18 06:55:41 +0200

    .

--- app/Modules/WhatsApp/Services/ConversationService.php
+++ app/Modules/WhatsApp/Services/ConversationService.php
@@ -72,7 +72,7 @@ class ConversationService
     {
         return Conversation::where('status_id', 0)->with(['contact' => function($q){
             return $q->with('last_message');
-        }])->get()->groupBy('conversation_id');
+        }])->get()->groupBy('conversation_id')->get();
     }
 
     public function getOld()
