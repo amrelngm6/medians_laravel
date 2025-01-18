e85756e286d4bfe6142b89d831a075f73c607c1a
unknown <amr_elngm6@yahoo.com>
2025-01-18 06:45:35 +0200

    .

--- app/Modules/WhatsApp/Migrations/2025_01_10_024150_create_wp_chat_members_table.php
+++ app/Modules/WhatsApp/Migrations/2025_01_10_024150_create_wp_chat_members_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('wp_participants');
+        Schema::dropIfExists('wp_chat_members');
     }
 };
