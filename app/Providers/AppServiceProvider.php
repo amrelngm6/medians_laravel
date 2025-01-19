d927bc6207d74a01ea520e5fbd773336fa5be770
unknown <amr_elngm6@yahoo.com>
2025-01-19 07:20:05 +0200

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use App\Services\RoleLoader;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Log;
 
 
 
@@ -46,9 +47,11 @@ class AppServiceProvider extends ServiceProvider
         if (Schema::hasTable('modules')  )
         {
             $modules = DB::table('modules')
-                ->where('is_enabled', true)
+                ->where('is_enabled', 1)
                 ->pluck('provider');
                 
+            Log::info('Loaded Modules '. json_encode($modules));
+
             foreach ($modules as $providerClass) {
                 if (class_exists($providerClass)) {
                     $this->app->register($providerClass);
