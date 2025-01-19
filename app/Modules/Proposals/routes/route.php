0e52a04db4553f89eca16fbdad9053754d48fb51
unknown <amr_elngm6@yahoo.com>
2025-01-19 07:08:55 +0200

    .

--- app/Modules/Proposals/routes/route.php
+++ app/Modules/Proposals/routes/route.php
@@ -3,11 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Modules\Proposals\Controllers\ProposalController;
 
-Route::prefix('estimates')->group(function () {
-    Route::get('/', [ProposalController::class, 'index']);
-    Route::post('/', [ProposalController::class, 'store']);
-    Route::get('{id}', [ProposalController::class, 'show']);
-    Route::put('{id}', [ProposalController::class, 'update']);
-    Route::delete('{id}', [ProposalController::class, 'destroy']);
+Route::prefix('proposals')->middleware(['web', 'auth:staff'])->group(function () {
+    Route::get('/', [ProposalController::class, 'index'])->name('Proposal');
+    Route::get('/create', [ProposalController::class, 'create'])->name('Proposal.create');
+    Route::get('{id}/edit', [ProposalController::class, 'edit'])->name('Proposal.edit');
+    Route::post('/store', [ProposalController::class, 'store'])->name('Proposal.store');
+    Route::post('{id}/update', [ProposalController::class, 'update'])->name('Proposal.update');
+    Route::delete('{id}', [ProposalController::class, 'destroy'])->name('Proposal.delete');
 });
 
+
+Route::prefix('items')->middleware(['web', 'auth:staff'])->group(function () {
+    Route::post('/itemRow', [ProposalController::class, 'itemRow'])->name('Proposal.item-row');
+});
+
+// Add Tab menu to projects Page
+Route::prefix('projects')->middleware(['web', 'auth:staff'])->group(function () {
+    Route::get('/{id}/proposals', [ProposalController::class, 'project'])->name('Projects.tabs.proposals');
+});
