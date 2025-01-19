5beca798f50a4da2fd5e06a130e3465fc0d48cfb
unknown <amr_elngm6@yahoo.com>
2025-01-19 08:42:13 +0200

    .

--- app/Modules/Proposals/Models/Proposal.php
+++ app/Modules/Proposals/Models/Proposal.php
@@ -11,6 +11,16 @@ class Proposal extends Model
 
     protected $fillable = ['business_id', 'title', 'content', 'model_type', 'model_id', 'user_type', 'user_id', 'date', 'expiry_date', 'currency_id', 'subtotal', 'discount_amount'. 'tax_amount', 'total', 'created_by', 'status_id'];
 
+
+    /**
+     * Relation with User as morphTo
+     */
+    public function user()
+    {
+        return $this->morphTo();
+    }
+
+
     /**
      * Estimates of Business
      */
