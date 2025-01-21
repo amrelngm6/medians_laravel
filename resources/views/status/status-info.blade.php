ed33a89fdbc1af98c6cd43806f028f1a2de14999
unknown <amr_elngm6@yahoo.com>
2025-01-21 08:05:42 +0200

    .

--- resources/views/status/status-info.blade.php
+++ resources/views/status/status-info.blade.php
@@ -2,6 +2,6 @@
                             data-content="Change Status">
                                 <small>Status</small>
                                 <a class="pb-2 open-modal text-primary block w-full " href="{{route('StatusList.edit_task_field',$model->id())}}" >
-                                    <i class='bx bx-power-off'></i> {{ $status->name }} <span class="h-10px inline-block rounded-full w-10px bg-{{$status->color}}"></span>
+                                    <i class='bx bx-power-off'></i> {{ $status->name ?? '' }} <span class="h-10px inline-block rounded-full w-10px bg-{{$status->color}}"></span>
                                 </a>
                             </div>
