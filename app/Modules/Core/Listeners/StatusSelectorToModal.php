0f355683606e6a18390ad41475c475c21144a643
unknown <amr_elngm6@yahoo.com>
2025-01-21 08:13:15 +0200

    .

--- app/Modules/Core/Listeners/StatusSelectorToModal.php
+++ app/Modules/Core/Listeners/StatusSelectorToModal.php
@@ -10,7 +10,7 @@ class StatusSelectorToModal
     public function handle($event)
     {
         $user = Auth::user();
-        $status = Status::default([$user->business_id ?? 0, 0])->find($event->model->status_id ?? 0);
+        $status = Status::default([$user->business_id ?? 0, 0])->where('status_id',$event->model->status_id ?? '0')->first();
         $model = $event->model ?? null;
         $event->context['components'][] = view('status.status-info', compact('status', 'model'))->render();
         return $event;
