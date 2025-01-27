3da7e42baedc8d73b17d227affd9965714807a09
unknown <amr_elngm6@yahoo.com>
2025-01-27 11:01:08 +0200

    Added AI Features to handle Task Checklist

--- app/Modules/Tasks/Controllers/TaskChecklistController.php
+++ app/Modules/Tasks/Controllers/TaskChecklistController.php
@@ -130,7 +130,7 @@ class TaskChecklistController extends Controller
         
         $task = $this->taskService->find($task_id); 
 
-        $message = 'could you give me json list for tasks to '.$task->description.' for my CRM in laravel. Please make the response valid json  with 6 items only , and as short as possible';
+        $message = 'could you give me json list for tasks to '.$task->description.' for my CRM in laravel. Please make the response valid json  with 6 items only, and two keys title and description , and as short as possible';
 
         $nlpService = new HuggFaceService;
         $response = $nlpService->generateText($message, $request->model);
@@ -149,7 +149,7 @@ class TaskChecklistController extends Controller
             foreach ($response as $key => $value) 
             {
                 $taskList['sort'] = $key;
-                $taskList['description'] = $value['task'] ?? ($value['description'] ?? ($value['name'] ?? ''));
+                $taskList['description'] = $value['task'] ?? ($value['description'] ?? ($value['name'] ?? ($value['title'] ?? '')));
 
                 $save = $this->taskChecklistService->createItem($taskList);
             }
