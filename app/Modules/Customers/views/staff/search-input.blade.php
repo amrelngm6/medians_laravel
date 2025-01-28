20c44fb274b988de9758ae34aca154beec0959ff
unknown <amr_elngm6@yahoo.com>
2025-01-28 10:16:36 +0200

    Handle Tasks page

--- app/Modules/Customers/views/staff/search-input.blade.php
+++ app/Modules/Customers/views/staff/search-input.blade.php
@@ -3,7 +3,7 @@
     rel="popover" data-toggle="popover" data-placement="top" data-trigger="hover" data-content="Filter by Staff Name" >
 
     <!-- <label for="assigned" class="control-label">Assigned To</label> -->
-    <select id="name-filter" name="staff_id" placeholder="A" data-live-search="true" data-ajax--url="{{route('Staff.search-input')}}" data-ajax--data-type="json" data-ajax--data-field="name" data-ajax--data-token="{{csrf_token()}}" data-ajax--data-search="name" data-ajax--data-value="id" data-ajax--data-text="name" data-ajax--data-minimum-input-length="2" data-ajax--data-delay="250" data-ajax--data-method="GET" data-ajax--data-cache="true" data-ajax--data-allow-clear="true" data-ajax--data-placeholder="Search by Staff Name" data-ajax--data-width="100%" data-ajax--data-theme="bootstrap" data-ajax--data-escape-markup="true" data-ajax--data-template-result='<span class="select2-results__option" role="option" aria-selected="false"><%= text %></span>' data-ajax--data-template-selection='<span class="select2-selection__rendered" id="select2-staff_id-container" title="<%= text %>"><%= text %></span>'
+    <select id="name-filter" name="staff_id" placeholder="A" data-live-search="true" 
         class="filter-on-change with-ajax border border-gray-300 form-control form-control-solid ">
         <option value=""></option>
     </select>
