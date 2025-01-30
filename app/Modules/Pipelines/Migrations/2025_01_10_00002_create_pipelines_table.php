95dc827fcf8557133a9771e1c923a104e626fbcd
unknown <amr_elngm6@yahoo.com>
2025-01-30 09:43:41 +0200

    Handle Pipelines Module

---
+++ app/Modules/Pipelines/Migrations/2025_01_10_00002_create_pipelines_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pipelines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('color', 50);
+            $table->integer('sort')->default(1);
+            $table->string('model', 191);
+            $table->integer('created_by');
+            $table->integer('business_id');
+            $table->timestamps();
+        });
+        
+        Schema::create('pipeline_stages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->integer('pipeline_id');
+            $table->integer('sort')->default(1);
+            $table->integer('created_by');
+            $table->integer('business_id');
+            $table->timestamps();
+        });
+        
+        Schema::create('pipeline_selected', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pipeline_id');
+            $table->morphs('model');
+            $table->integer('created_by');
+            $table->integer('business_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pipelines');
+        Schema::dropIfExists('pipeline_stages');
+        Schema::dropIfExists('pipeline_selected');
+    }
+};
