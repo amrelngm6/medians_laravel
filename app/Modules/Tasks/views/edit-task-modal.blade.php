9bb919a509665e1fbb84dd42f98529ff57de3643
unknown <amr_elngm6@yahoo.com>
2025-02-04 09:40:17 +0200

    Handle Deals Taaks

--- app/Modules/Tasks/views/edit-task-modal.blade.php
+++ app/Modules/Tasks/views/edit-task-modal.blade.php
@@ -1,5 +1,5 @@
 
-<div class="modal fade  active" id="edit-task-modal" tabindex="-1" >
+<div class="modal fade show active" id="edit-task-modal" tabindex="-1" >
     <!--begin::Modal dialog-->
     <div class="modal-dialog modal-dialog-centered mw-650px">
         <!--begin::Modal content-->
