5da915fcbc891308bac25baca1e29078c498be1f
unknown <amr_elngm6@yahoo.com>
2025-02-05 11:21:05 +0200

    Handle Reminders Module

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,10 @@
 self.addEventListener("push", function (event) {
-    console.log("Push event received:", event);
-    console.log("Push event received:", event.data);
-    console.log("Push event received:", event.data.text());
 
     let data = {};
     if (event.data) {
-        data = event.data.text();
+        // let res = event.data.text();
+        // data = JSON.parse(res);
+        data = event.data.json();
     }
 
     const title = data.title || "Reminder Notification";
@@ -20,29 +19,6 @@ self.addEventListener("push", function (event) {
         self.registration.showNotification(title, options)
     );
 });
-
-// self.addEventListener('push', function(event) {
-//     // const payload = event.data ? event.data.json() : {};
-
-//     // const title = payload.title || 'Task Reminder';
-//     // const options = {
-//     //     body: payload.body || 'You have a task due.',
-//     //     icon: '/icon.png',
-//     //     data: payload.data || {}
-//     // };
-//     const obj = event.data.json();
-
-//     if (obj.action === "subscribe" || obj.action === "unsubscribe") {
-//       fireNotification(obj, event);
-//       port.postMessage(obj);
-//     } else if (obj.action === "init" || obj.action === "chatMsg") {
-//       port.postMessage(obj);
-//     }
-
-//     // event.waitUntil(
-//     //     self.registration.showNotification(title, options)
-//     // );
-// });
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
