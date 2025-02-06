e26bf5591f1c2a4b5953ce158e6059dcbea9f539
unknown <amr_elngm6@yahoo.com>
2025-02-06 04:56:54 +0200

    Handle Reminders Module

--- resources/views/status/status-info-ticket.blade.php
+++ resources/views/status/status-info-ticket.blade.php
@@ -1,4 +1,4 @@
-                            <div class="">
+                            <div class="flex-column-auto">
                                 <small>Status</small>
                                 <a class="pb-2 open-modal text-primary block w-full " href="{{route('StatusList.edit_field_ticket',$model->id())}}" >
                                     <i class='bx bx-power-off'></i> {{ $status->name ?? '' }} <span class="h-10px inline-block rounded-full w-10px bg-{{$status->color ?? ''}}"></span>
