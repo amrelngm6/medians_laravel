c2d984f3d8dfa50d1008a26bc2ff4affe3d9f362
unknown <amr_elngm6@yahoo.com>
2025-02-06 04:30:39 +0200

    Handle Reminders Module

--- app/Modules/Actions/views/reminders/modal-info.blade.php
+++ app/Modules/Actions/views/reminders/modal-info.blade.php
@@ -6,7 +6,7 @@
                                         data-content="Reminders of spent time for this task"
                                         class="text-primary fw-bold fs-7 pb-6 open-modal"
                                         href="{{route('Reminder.showList', ['model_id'=>$model->id() ?? '0', 'model_type' => get_class($model)])}}"
-                                        data-modal="#new-timesheet-modal">
+                                        data-modal="#new-reminder-modal">
                                         <i class="bx bx-log "></i> History
                                     </a>
                                 </div>
@@ -22,28 +22,3 @@
                                         class="h-10px inline-block rounded-full w-10px "></span>
                                 </a>
                             </div>
-@if ($timesheet)
-<script>
-let interval;
-jQuery(document).ready(function(){
-
-    // Calculate the time difference between the start time and now using moment.js
-    moment.tz.setDefault('{{env('APP_TIMEZONE')}}');
-
-    if (interval)
-    {
-        clearInterval(interval)   
-    }
-    interval = setInterval(async () => {
-        let startTime = moment("{{$timesheet->start}}", "YYYY-MM-DD hh:mm:ss");
-        let now = moment.utc();
-        let duration = moment.duration(now.tz('{{env('APP_TIMEZONE')}}').diff(startTime));
-        let hours = String(duration.hours()).padStart(2, '0');
-        let minutes = String(duration.minutes()).padStart(2, '0');
-        let seconds = String(duration.seconds()).padStart(2, '0');
-        jQuery('#timer-track').html(`${hours}:${minutes}:${seconds}`)
-    }, 1000)
-})
-
-</script>
-@endif
