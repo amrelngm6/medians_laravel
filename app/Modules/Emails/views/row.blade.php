9c3c2b6d5947a7834840ce8a300f63c06ff708f1
unknown <amr_elngm6@yahoo.com>
2025-02-06 21:56:47 +0200

    Handle Reminders Module

--- app/Modules/Emails/views/row.blade.php
+++ app/Modules/Emails/views/row.blade.php
@@ -78,7 +78,7 @@
                                         </svg>
                                     </a>
                                     
-                                    <a class="ml-5 flex py-2 px-1 items-center justify-center ajax-load" data-element="#messages-list" href="{{route('EmailAccount.fetch', $account->id)}}?folder={{$folder->id ?? 0}}&days=30">
+                                    <a class="ml-5 flex py-2 px-1 items-center justify-center ajax-load" data-element="#messages-list" href="{{route('EmailAccount.fetch', $account->id)}}?folder={{$folder->id ?? 0}}&days={{$account->field('since_days')->first()->value ?? 0}}">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                             stroke-linecap="round" stroke-linejoin="round" data-lucide="refresh-cw"
