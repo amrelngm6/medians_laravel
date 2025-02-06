af94c5069d13beacde884612a6acee312beb4482
unknown <amr_elngm6@yahoo.com>
2025-02-06 14:21:34 +0200

    Handle Reminders Module

--- app/Modules/Emails/routes/route.php
+++ app/Modules/Emails/routes/route.php
@@ -8,11 +8,11 @@ Route::prefix('email_accounts')->middleware(['web', 'auth:staff,superadmin'])->g
     Route::get('/index', [EmailAccountController::class, 'index'])->name('EmailAccounts');
     Route::get('/{account}/filter', [EmailAccountController::class, 'filter'])->name('EmailAccount.filter');
     Route::get('/{account}/fetch', [EmailAccountController::class, 'fetch'])->name('EmailAccount.fetch');
-    Route::get('/{account}/fetchFolders', [EmailAccountController::class, 'fetchFolders'])->name('EmailAccount.fetchFolders');
     Route::get('create', [EmailAccountController::class, 'create'])->name('EmailAccount.create');
     Route::get('{id}/show', [EmailAccountController::class, 'show'])->name('EmailAccount.show');
     Route::get('{id}/message/{msg_id}', [EmailAccountController::class, 'showMessage'])->name('EmailAccount.showMessage');
     Route::get('{id}/edit', [EmailAccountController::class, 'edit'])->name('EmailAccount.edit');
+    Route::post('/{account}/fetchFolders', [EmailAccountController::class, 'fetchFolders'])->name('EmailAccount.fetchFolders');
     Route::post('/', [EmailAccountController::class, 'store'])->name('EmailAccount.store');
     Route::post('{id}', [EmailAccountController::class, 'update'])->name('EmailAccount.update');
     Route::delete('{id}', [EmailAccountController::class, 'destroy'])->name('EmailAccount.delete');
