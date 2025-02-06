aa34f54089743f4c311f4a9ccb4c0bc539f4663d
unknown <amr_elngm6@yahoo.com>
2025-02-06 14:33:28 +0200

    Handle Reminders Module

--- app/Modules/Emails/Services/EmailAccountService.php
+++ app/Modules/Emails/Services/EmailAccountService.php
@@ -133,7 +133,7 @@ class EmailAccountService
     {
         $user = Auth::user();
 
-        $items = EmailFolder::withCount('messages')->forBusiness($user->business_id ?? 0)->forEmail($account->email)->orderBy('messages_count', 'DESC')->get();
+        $items = EmailFolder::forBusiness($user->business_id ?? 0)->forEmail($account->email)->withCount('messages')->orderBy('messages_count', 'ASC')->get();
 
         return $items;
     }
