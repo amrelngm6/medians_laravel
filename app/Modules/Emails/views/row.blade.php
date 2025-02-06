39a243d354e2650254c258ca4f4ee105d5e6d614
unknown <amr_elngm6@yahoo.com>
2025-02-06 22:56:19 +0200

    Handle Reminders Module

--- app/Modules/Emails/views/row.blade.php
+++ app/Modules/Emails/views/row.blade.php
@@ -62,20 +62,16 @@
                                         </svg>
                                     </a>
                                     
-                                    <a class="ml-5 flex py-2 px-1 items-center justify-center open-modal"  href="{{route('EmailAccount.settings', $account->id)}}">
-                                        <i class='bx bxs-user-detail'></i>
+                                    <a class="ml-5 flex py-2 px-1 items-center justify-center open-modal"  href="{{route('EmailAccount.settings', $account->id)}}"
+                                        rel="popover" data-toggle="popover" data-placement="top" data-trigger="hover"
+                                        data-content="Email account Settings">
+                                        <i class='bx bx-cog fs-2'></i>
                                     </a>
                                     
-                                    <a class="ml-5 flex py-2 px-1 items-center justify-center open-modal"  href="{{route('EmailAccount.edit', $account->id)}}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" data-lucide="settings"
-                                            class="lucide lucide-settings stroke-1.5 h-6 w-6">
-                                            <path
-                                                d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z">
-                                            </path>
-                                            <circle cx="12" cy="12" r="3"></circle>
-                                        </svg>
+                                    <a class="ml-5 flex py-2 px-1 items-center justify-center open-modal"  href="{{route('EmailAccount.edit', $account->id)}}"
+                                        rel="popover" data-toggle="popover" data-placement="top" data-trigger="hover"
+                                        data-content="IMAP server info">
+                                        <i class='bx bxs-user-detail fs-2'></i>
                                     </a>
                                     
                                     <a class="ml-5 flex py-2 px-1 items-center justify-center ajax-load" data-element="#messages-list" href="{{route('EmailAccount.fetch', $account->id)}}?folder={{$folder->id ?? 0}}&days={{$account->field('since_days')->first()->value ?? 7 }}">
