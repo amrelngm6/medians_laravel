ed8ac00a0088af57d248282afad14945b196a68d
unknown <amr_elngm6@yahoo.com>
2025-02-06 05:18:56 +0200

    Handle Reminders Module

--- app/Modules/Actions/views/reminders/modal-info.blade.php
+++ app/Modules/Actions/views/reminders/modal-info.blade.php
@@ -3,7 +3,7 @@
                                     <small class="w-full">Reminders</small>
                                     <a style="line-height:5px; " rel="popover" data-toggle="popover"
                                         data-placement="top" data-trigger="hover"
-                                        data-content="Reminders of spent time for this task"
+                                        data-content="Reminders of spent time for this {{class_basename($model)}}"
                                         class="text-primary fw-bold fs-7 pb-6 open-modal"
                                         href="{{route('Reminder.showList', ['model_id'=>$model->id() ?? '0', 'model_type' => get_class($model)])}}"
                                         data-modal="#new-reminder-modal">
@@ -16,7 +16,7 @@
                                     href="{{route('Reminder.createForModel', ['model_id'=>$model->id() ?? '0', 'model_type' => get_class($model)])}}?_token={{csrf_token()}}"
                                     id="task-reminder"
                                     class="open-modal btn btn-primary btn-sm">
-                                    <i class='bx bx-time-five'></i> Add <span
+                                    <i class='bx bx-time-five'></i> Remind me <span
                                         class="h-10px inline-block rounded-full w-10px "></span>
                                 </a>
                             </div>
