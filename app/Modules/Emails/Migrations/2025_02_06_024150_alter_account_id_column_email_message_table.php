4f2d95e25cf8c70710c3390c93e1b8d7db7d3687
unknown <amr_elngm6@yahoo.com>
2025-02-06 19:25:03 +0200

    Handle Reminders Module

---
+++ app/Modules/Emails/Migrations/2025_02_06_024150_alter_account_id_column_email_message_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('email_messages', function (Blueprint $table) {
+            $table->unsignedBigInteger('account_id')->nullable()->after('model_id'); // Add the column
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('email_messages', function (Blueprint $table) {
+            $table->dropColumn('account_id');   // Remove the column
+        });
+    }
+};
