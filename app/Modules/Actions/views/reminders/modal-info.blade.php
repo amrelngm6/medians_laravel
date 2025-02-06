9fe3cd691d3987a7028030646b408def6b8b3dfa
unknown <amr_elngm6@yahoo.com>
2025-02-06 05:19:38 +0200

    Handle Reminders Module

--- app/Modules/Actions/views/reminders/modal-info.blade.php
+++ app/Modules/Actions/views/reminders/modal-info.blade.php
@@ -3,7 +3,7 @@
                                     <small class="w-full">Reminders</small>
                                     <a style="line-height:5px; " rel="popover" data-toggle="popover"
                                         data-placement="top" data-trigger="hover"
-                                        data-content="Reminders of spent time for this {{class_basename($model)}}"
+                                        data-content="Reminders of this {{class_basename($model)}}"
                                         class="text-primary fw-bold fs-7 pb-6 open-modal"
                                         href="{{route('Reminder.showList', ['model_id'=>$model->id() ?? '0', 'model_type' => get_class($model)])}}"
                                         data-modal="#new-reminder-modal">
@@ -12,7 +12,7 @@
                                 </div>
                                 
                                 <a rel="popover" data-toggle="popover" data-placement="top" data-trigger="hover"
-                                    data-content="Set new Reminder for this task" 
+                                    data-content="Set new Reminder for this {{class_basename($model)}}" 
                                     href="{{route('Reminder.createForModel', ['model_id'=>$model->id() ?? '0', 'model_type' => get_class($model)])}}?_token={{csrf_token()}}"
                                     id="task-reminder"
                                     class="open-modal btn btn-primary btn-sm">
