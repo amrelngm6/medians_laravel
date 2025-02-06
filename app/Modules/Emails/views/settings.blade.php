63ea27ae1cd86f314f391146db62c0a0cbf91e81
unknown <amr_elngm6@yahoo.com>
2025-02-06 21:36:39 +0200

    Handle Reminders Module

--- app/Modules/Emails/views/settings.blade.php
+++ app/Modules/Emails/views/settings.blade.php
@@ -44,76 +44,13 @@
                             </div>
                             <div class="d-flex align-items-center w-full">
                                 <div class="d-flex flex-column  fv-row w-full">
-                                    <input placeholder="IMAP host " name="imap_host" value="{{$account->imap_host}}" type="number" min="1" max="100" required class="form-control form-control-solid" />
+                                    <input placeholder="Load since x days" name="imap_host" value="{{$account->imap_host}}" type="number" min="1" max="100" required class="form-control form-control-solid" />
                                 </div>
                             </div>
                         </div>
                     </div>
                     <!--end::Input group-->
 
-                    <!--begin::Input group-->
-                    <div class="mb-15 fv-row">
-                        <div class="d-flex flex-stack">
-                            <div class="fw-semibold me-5">
-                                <label class="fs-6  required">IMAP port</label>
-                                <div class="fs-7 text-muted">IMAP port for your domain ex: 993 </div>
-                            </div>
-                            <div class="d-flex align-items-center">
-                                <div class="d-flex flex-column  fv-row">
-                                    <input placeholder="IMAP port " name="imap_port" value="{{$account->imap_port}}" type="number" required class="form-control form-control-solid" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end::Input group-->
-                    
-
-                    <!--begin::Input group-->
-                    <div class="mb-15 fv-row">
-                        <div class="d-flex flex-stack">
-                            <div class="fw-semibold me-5">
-                                <label class="fs-6  required">IMAP Username</label>
-                                <div class="fs-7 text-muted">IMAP username is your email <br />ex: admin@mediansai.com </div>
-                            </div>
-                            <div class="d-flex align-items-center">
-                                <div class="d-flex flex-column  fv-row">
-                                    <input placeholder="IMAP username (ex: email@domain.com)" name="imap_username" value="{{$account->imap_username}}" required class="form-control form-control-solid" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end::Input group-->
-
-                    
-                    <!--begin::Input group-->
-                    <div class="mb-15 fv-row">
-                        <div class="d-flex flex-stack">
-                            <div class="fw-semibold me-5">
-                                <label class="fs-6  required">IMAP password</label>
-                                <div class="fs-7 text-muted">IMAP password is your email login password  </div>
-                            </div>
-                            <div class="d-flex align-items-center">
-                                <div class="d-flex flex-column  fv-row">
-                                    <input placeholder="IMAP login password" name="imap_password" value="" required class="form-control form-control-solid" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end::Input group-->
-
-                    <!--begin::Notice-->
-                    <div class="notice d-flex bg-light-warning rounded border-warning border border-dashed  p-6">
-                        <!--begin::Wrapper-->
-                        <div class="d-flex flex-stack flex-grow-1 ">
-                            <!--begin::Content-->
-                            <div class=" fw-semibold">
-                                <div class="fs-6 text-gray-700 "> We don't save the Password, you have to enter your password for each time you fetch your Email messages</div>
-                            </div>
-                        </div>
-                        <!--end::Wrapper-->
-                    </div>
-                    <!--end::Notice-->
-                    
 
                     <!--begin::Actions-->
                     <div class="text-center pt-10">
