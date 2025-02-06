0e13e1028306b53cad34237944e73ccffaa16c0e
unknown <amr_elngm6@yahoo.com>
2025-02-06 16:49:26 +0200

    Handle Reminders Module

--- app/Modules/Emails/Services/SendMail.php
+++ app/Modules/Emails/Services/SendMail.php
@@ -8,6 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Address;
 
 use App\Modules\Emails\Models\EmailMessage;
 
