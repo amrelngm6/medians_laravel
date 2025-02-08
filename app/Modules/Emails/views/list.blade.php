8dc16298d408c2e18e86dd2637197d4ebb944c6c
unknown <amr_elngm6@yahoo.com>
2025-02-08 12:35:26 +0200

    Handle Reminders Module

--- app/Modules/Emails/views/list.blade.php
+++ app/Modules/Emails/views/list.blade.php
@@ -44,11 +44,24 @@
                             <div class="card-body px-0">
                                 <a href="{{route('EmailMessage.compose', $account->id)}}" data-modal="#new-mail-modal" class="open-modal mx-auto w-64 btn btn-primary transition duration-200  shadow-md cursor-pointer "><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="edit" class="lucide lucide-edit stroke-1.5 mr-2 h-4 w-4"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.12 2.12 0 0 1 3 3L12 15l-4 1 1-4Z"></path></svg>
                                     Compose</a>
-                                <div class="mt-6 border-t border-white/10 pt-6 dark:border-darkmode-400 ">
+                                <div class="border-t border-white/10 pt-6 dark:border-darkmode-400 ">
+                                    
+                                    
+                                    <div class="w-full my-4  flex gap-2">
+                                        <span class="p-2 w-full text-primary"> Folders ( {{count($folders)}} ) </span>
+                                        
+
+                                        <div class="btn-group mb-1" rel="popover" data-toggle="popover" data-placement="top" data-trigger="hover"
+                                            data-content="Create new folder">
+                                            <a class="dropdown-item open-modal fs-2 ps-4 font-semibold textprimary"
+                                                href="{{route('EmailAccount.createFolder', $account->id)}}?_token={{csrf_token()}}"><i
+                                                    class="bx bx-folder-plus text-danger"></i></a>
+                                        </div>
+                                    </div>
                                     @foreach ($folders as $folder)
                                     <div class="w-full flex gap-2">
                                         <i class="bx bx-start"></i>
-                                        <a class="{{$folder->id == request()->get('folder') ? 'text-danger' : 'text-dark-blue'}} w-full text-hover-info mt-2 flex gap-4 items-center rounded-md px-3 py-2 fs-4 "
+                                        <a class="{{$folder->id == request()->get('folder') ? 'text-danger' : 'text-dark-blue'}} w-full text-hover-info mt-2 flex gap-4 items-center rounded-md py-2 fs-4 "
                                             href="{{route('EmailAccount.show', $account->id)}}?folder={{$folder->id}}">
                                             {{$folder->name}} ({{$folder->messages_count}})</a>
                                         <div class="dropdown show-child relative mt-2">
@@ -65,20 +78,12 @@
 
                                 </div>
                                 <div class="mt-4 border-t border-white/10 pt-4 text-white dark:border-darkmode-400">
-                                @foreach($priorities as $priority)
+                                    @foreach($priorities as $priority)
                                     <a class="flex items-center text-truncate px-3 py-2" href="?priority_id={{$priority->priority_id}}">
                                         <div class="mr-5 w-15px h-15px rounded-circle bg-{{$priority->color}}"></div>
                                         {{$priority->name}}
                                     </a>
                                     @endforeach
-                                    
-                                    <a class="mt-2 flex items-center  rounded-md px-3 py-2 ajax-link" href="{{route('EmailAccount.fetchFolders', $account->id)}}?_token={{csrf_token()}}">
-                                        Refresh Folders
-                                    </a>
-                                    
-                                    <a class="mt-2 flex items-center  rounded-md px-3 py-2 " href="{{route('EmailAccount')}}">
-                                        Show ALL
-                                    </a>
                                     <a class="mt-2 flex items-center text-truncate rounded-md px-3 py-2 open-modal" href="{{route('Priority.create')}}?model=EmailAccount">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
                                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -87,6 +92,9 @@
                                             <path d="M12 5v14"></path>
                                         </svg>
                                         Add New Label
+                                    </a>
+                                    <a class="border-t mt-4 pt-4 flex items-center fw-bold rounded-md px-3" href="{{route('EmailAccount')}}">
+                                        Show ALL Accounts
                                     </a>
                                 </div>
                             </div>
