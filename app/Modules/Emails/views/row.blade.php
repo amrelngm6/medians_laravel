a9bb9981b8e39415a1bb357272f76c3e6e52ab21
unknown <amr_elngm6@yahoo.com>
2025-02-08 12:04:16 +0200

    Handle Reminders Module

--- app/Modules/Emails/views/row.blade.php
+++ app/Modules/Emails/views/row.blade.php
@@ -78,7 +78,7 @@
                     <a data-switch="add-star{{$message->id}}" id="remove-star{{$message->id}}" href="{{route('EmailMessage.update', $message->id)}}?_token={{csrf_token()}}&favourite=1" class="{{$message->favourite ? 'hidden' : ''}} ajax-link switch-view ml-4  pt-1 px-1 flex-none items-center justify-center text-slate-400" href="#">
                         <i class='bx bx-star fs-3'></i>
                     </a>
-                    <a data-switch="remove-star{{$message->id}}" id="add-star{{$message->id}}" href="{{route('EmailMessage.update', $message->id)}}?_token={{csrf_token()}}&favourite=0" class="{{!$message->favourite ? 'hidden' : ''}} switch-view  ml-4  pt-1 px-1 flex-none items-center justify-center text-slate-400" href="#">
+                    <a data-switch="remove-star{{$message->id}}" id="add-star{{$message->id}}" href="{{route('EmailMessage.update', $message->id)}}?_token={{csrf_token()}}&favourite=0" class="{{!$message->favourite ? 'hidden' : ''}} ajax-link switch-view ml-4  pt-1 px-1 flex-none items-center justify-center text-slate-400" href="#">
                         <i class='bx bxs-star text-danger fs-3'></i>
                     </a>
                     <div class="ml-3 text-truncate font-medium" rel="popover" data-toggle="popover" data-placement="top"
