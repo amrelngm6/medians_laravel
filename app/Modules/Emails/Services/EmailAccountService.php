ecffd4feaa38fde6b6c1bbdab30f4929e6e02587
unknown <amr_elngm6@yahoo.com>
2025-02-08 12:22:01 +0200

    Handle Reminders Module

--- app/Modules/Emails/Services/EmailAccountService.php
+++ app/Modules/Emails/Services/EmailAccountService.php
@@ -83,7 +83,7 @@ class EmailAccountService
         $folder = $this->client->getFolder($folderName ?? 'INBOX');
 
         if (!$folder)
-            throw "Folder not found at Email Server";
+            throw new \Throwable("Folder not found at Email Server");
         
         $messages = $folder->query()->since(now()->subDays($days))->limit($limit)->get();
 
